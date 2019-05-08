09bf3abcd8bb58882826d9f72642e48dc0bf100c
Lance Ivy <lance@cainlevy.net>
2019-05-08 14:10:12 -0700

    use RequestStore to cache policies

--- lib/pundit/cache.rb
+++ lib/pundit/cache.rb
@@ -1,8 +1,22 @@
-require "pundit/cache/version"
+require 'request_store'
+require 'pundit/cache/version'
 
 module Pundit
   module Cache
-    class Error < StandardError; end
-    # Your code goes here...
+    # the policy result for a given user and record should not change during a request
+    def cache(method_name)
+      wrapped_name = "_uncached_#{method_name}"
+      alias_method wrapped_name, method_name
+      class_eval <<-RUBY, __FILE__, __LINE__ + 1
+        def #{method_name}
+          cache_key = "\#{user&.to_global_id}|\#{record&.to_global_id}|#{method_name}"
+          if RequestStore.store[cache_key].nil?
+            RequestStore.store[cache_key] = #{wrapped_name}
+          else
+            RequestStore.store[cache_key]
+          end
+        end
+      RUBY
+    end
   end
 end
--- test/pundit/cache_test.rb
+++ test/pundit/cache_test.rb
@@ -1,11 +1,59 @@
-require "test_helper"
+require 'test_helper'
 
-class Pundit::CacheTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Pundit::Cache::VERSION
-  end
+module Pundit
+  class CacheTest < Minitest::Test
+    ApplicationPolicy = Struct.new(:user, :record) do
+      extend Pundit::Cache
+    end
 
-  def test_it_does_something_useful
-    assert false
+    User = Struct.new(:id) do
+      def to_global_id
+        "gid://myapp/User/#{id.to_s}"
+      end
+    end
+
+    Record = Struct.new(:id) do
+      def to_global_id
+        "gid://myapp/Record/#{id.to_s}"
+      end
+    end
+
+    class LuckyPolicy < ApplicationPolicy
+      def lucky?
+        rand(2) == 1
+      end
+      cache :lucky?
+    end
+
+    class UserPolicy < ApplicationPolicy
+      def even?
+        user && user.id.even?
+      end
+      cache :even?
+    end
+
+    class RecordPolicy < ApplicationPolicy
+      def odd?
+        record && record.id.odd?
+      end
+      cache :odd?
+    end
+
+    def test_caching
+      user = User.new(1)
+      record = Record.new(9)
+
+      assert_equal 1, 100.times.map{ LuckyPolicy.new(user, record).lucky? }.uniq.length
+    end
+
+    def test_varying_user
+      refute UserPolicy.new(User.new(1), Record.new(9)).even?
+      assert UserPolicy.new(User.new(2), Record.new(9)).even?
+    end
+
+    def test_varying_record
+      assert RecordPolicy.new(User.new(1), Record.new(7)).odd?
+      refute RecordPolicy.new(User.new(1), Record.new(8)).odd?
+    end
   end
 end
